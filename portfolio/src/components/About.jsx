cb9e12e64a2d0b75ab3c6c76baa75c9933ce25e4
rahul <129116227+RahulSinghKathayat@users.noreply.github.com>
2025-03-10 23:49:41 +0530

    npm

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () =>{
                 <div></div>
                 </div>
                 <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-                    <div className="sm:text-right text-4xl font-bold">
+                    <div className="sm:text-right text-3xl font-bold">
                         <p className="flex text-left">Hi. I'm Rahul Kathayat, nice to meet you. Please take a look around.</p>
                         </div>
                         <div>
